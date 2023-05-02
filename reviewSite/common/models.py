ad03748bc41f88e01ad46058895bef1765a8cea4
kmus1232 <kmus1232@gmail.com>
2023-05-02 20:32:28 +0900

    test

--- reviewSite/common/models.py
+++ reviewSite/common/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .validators import validate_no_special_characters
 
-
 class User(AbstractUser):
     nickname = models.CharField(
         max_length=15,
@@ -11,3 +10,4 @@ class User(AbstractUser):
         validators=[validate_no_special_characters],
         error_messages={'unique': '이미 사용 중인 닉네임입니다.'},
     )
+
