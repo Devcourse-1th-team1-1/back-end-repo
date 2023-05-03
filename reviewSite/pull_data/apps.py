cf0e30e25a4260d820f78884e271527fa080db78
kmus1232 <kmus1232@gmail.com>
2023-05-03 18:11:49 +0900

    불필요한 파일 삭제 - Revert "feat/giho"
    This reverts commit 040aba163acf8b849302cb0b707f2efdd507b429.

--- reviewSite/pull_data/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PullDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pull_data'
--- reviewSite/pull_data/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    movie_title = models.CharField(max_length=50)
-    good_img = models.ImageField(verbose_name="positive_cloud",null=True,upload_to='good_img/')
-    bad_img = models.ImageField(verbose_name="negative_cloud",null=True,upload_to='bad_img/')
-    poster_img = models.ImageField(verbose_name="poster",upload_to='movie_poster/')
