dfd632e1207ef0ed124ae86d7c5ee382bbd9a182
heewoneha <jhjmo0719h@naver.com>
2023-05-03 10:14:29 +0900

    fix: 백업과정에서 생긴 text 변경

--- reviewSite/common/models.py
+++ reviewSite/common/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .validators import validate_no_special_characters
 
-# Create your models here.
 
 class User(AbstractUser):
     nickname = models.CharField(
