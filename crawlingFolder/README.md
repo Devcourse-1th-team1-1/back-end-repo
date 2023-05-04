b30af5de577aa392326bf9bda6fe2f71b8456666
HaJunYoo <hajuny129@gmail.com>
2023-05-04 15:54:06 +0900

    ✨ feat: rm cached

---
+++ crawlingFolder/README.md
@@ -0,0 +1,27 @@
+# crawling-repo
+github action 과 airflow를 사용해서 크롤링을 해보자
+
+### reference
+- git action 사용법
+    - https://zzsza.github.io/development/2020/06/06/github-action/
+    
+- git action with chromium
+    - https://morioh.com/p/9202cb0630ea
+    - https://github.com/jsoma/selenium-github-actions?ref=morioh.com&utm_source=morioh.com
+
+
+- Implicit wait: https://pythondocs.net/selenium/%EC%85%80%EB%A0%88%EB%8B%88%EC%9B%80-wait-%EA%B0%9C%EB%85%90-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0-implicitly-wait-vs-explicitly-wait/
+
+- Dockerization
+    - https://joolog.tistory.com/entry/Docker-chrome-%EC%84%A4%EC%B9%98-%ED%95%B4%EB%B3%B4%EA%B8%B0
+    - https://blog.naver.com/PostView.naver?blogId=llovemilton&logNo=222540262715&parentCategoryNo=&categoryNo=37&viewDate=&isShowPopularPosts=true&from=search
+
+- Github Actions setup
+    - https://github.com/marketplace/actions/setup-chromedriver
+    - https://blog.joonas.io/158
+    - https://velog.io/@king/weather-alert
+
+- Git Action permission and token
+    - https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/enabling-features-for-your-repository/managing-github-actions-settings-for-a-repository
+    - https://docs.github.com/en/actions/security-guides/automatic-token-authentication#permissions-for-the-github_token
+    - https://stackoverflow.com/questions/70538793/remote-write-access-to-repository-not-granted-fatal-unable-to-access
---
+++ crawlingFolder/movie_ranking_info.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os 
+
+
+class MovieScraper:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.movies = []
+    
+    def get_movies(self, url):
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        parent_tag = soup.find_all(class_='css-1qq59e8')
+        netflix_tag = parent_tag[2]
+
+        for idx, movie_li in enumerate(netflix_tag.find_all(class_='css-8y23cj')):
+            movie_link = movie_li.find('a')['href']
+            movie_img = movie_li.find('img')['src']
+            movie_title = movie_li.find(class_='css-5yuqaa').text
+            movie_mean_rate = movie_li.find(class_='average css-xgmur2').find_all('span')[1].text
+
+            movie = dict()
+            movie["rank"] = idx + 1
+            movie["title"] = movie_title
+            movie["mean_rate"] = movie_mean_rate
+            movie["movies_link"] = self.base_url + movie_link
+            movie["img"] = movie_img
+            
+            self.get_movie_details(movie)
+            self.movies.append(movie)
+            print(movie)
+        
+
+    def get_movie_details(self, movie):
+        movie_url = movie["movies_link"]
+        response = requests.get(movie_url)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        parent_tag = soup.find_all('article')[0]
+        title = parent_tag.find(class_='css-wvh1uf-Summary eokm2781').contents[0].strip()
+        year_country_genre = parent_tag.find_all(class_='css-1t00yeb-OverviewMeta eokm2782')[0].text.split('·')
+        year = year_country_genre[0].strip()
+        country = year_country_genre[1].strip()
+        genre = year_country_genre[2].strip()
+        
+        try:
+            time_regulation = parent_tag.find_all(class_='css-1t00yeb-OverviewMeta eokm2782')[1].text.split('·')
+            time = time_regulation[0].strip()
+            regulation = time_regulation[1].strip()
+        except IndexError:
+            time = "Unknown"
+            regulation = "Unknown"
+
+        description = parent_tag.find(class_='css-kywn6v-StyledText eb5y16b1').text.strip()
+
+        movie["year"] = year
+        movie["country"] = country
+        movie["genre"] = genre
+        movie["time"] = time
+        movie["regulation"] = regulation
+        movie["description"] = description
+
+    def save_to_csv(self, file_name):
+        df = pd.DataFrame(self.movies)
+        df.to_csv(file_name, index=False, encoding='utf-8-sig')
+
+
+if __name__ == "__main__":
+    base_url = "https://pedia.watcha.com"
+    ranking_url = "https://pedia.watcha.com/ko-KR"
+    scraper = MovieScraper(base_url)
+    scraper.get_movies(ranking_url)
+    
+    output_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+        
+    file_path = os.path.join(output_folder, "movie_rankings.csv")
+    scraper.save_to_csv(file_path)
+    print(file_path)
