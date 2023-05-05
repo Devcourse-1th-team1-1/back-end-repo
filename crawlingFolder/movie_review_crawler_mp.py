343fa20ad97d97c89a576dd3900325c4f9ad6076
HaJunYoo <hajuny129@gmail.com>
2023-05-05 15:04:43 +0900

    ✨ feat: mp adaptation

--- crawlingFolder/movie_review_crawler_mp.py
+++ crawlingFolder/movie_review_crawler_mp.py
@@ -124,7 +124,7 @@ class ReviewScraper:
             # print(f"현재까지 reviews: {len(self.reviews)}")
         
         # driver.quit()
-        print(f"Total reviews of {self.movie_title} : {len(self.reviews)}")
+        print(f"Total reviews: {self.movie_title} {len(self.reviews)}")
             
 
     def save_to_csv(self, movie_title, file_name):
@@ -170,6 +170,19 @@ def driver_get(executable_path):
     driver = webdriver.Chrome(driver_path, options=chrome_options)
     return driver
 
+def scrape_reviews(movie_url, movie_title, chrome_driver_path):
+    
+    chrome_driver = driver_get(chrome_driver_path)
+    scraper = ReviewScraper(movie_url=movie_url, driver=chrome_driver, executable_path=chrome_driver_path, movie_title=movie_title)
+    scraper.get_reviews()
+    cnt = 0
+    while len(scraper.reviews) < 5 and cnt < 3:
+        scraper = ReviewScraper(movie_url=movie_url, driver=chrome_driver, executable_path=chrome_driver_path, movie_title=movie_title)
+        scraper.get_reviews()
+        cnt += 1
+    chrome_driver.quit()
+    return scraper.reviews
+
 
 
 if __name__ == "__main__":
@@ -179,37 +192,35 @@ if __name__ == "__main__":
     input_data = os.path.join(input_folder, "movie_rankings.csv")
     movie_info = pd.read_csv(input_data)
     
-    chrome_driver = driver_get(chrome_driver_path)
-    all_reviews = []
+    pool = multiprocessing.Pool(processes=2)
+    results = []
+    # results = pool.map(scrape_reviews, [(row['movies_link'], row['title'], chrome_driver_path) for _, row in movie_info.iterrows()])[0]
     for idx, row in movie_info.iterrows():
-        movie_title = row['title']
         movie_url = row['movies_link']
-        scraper = ReviewScraper(movie_url=movie_url, driver = chrome_driver,executable_path=chrome_driver_path,
-                                    movie_title=movie_title)
-        scraper.get_reviews()
+        movie_title = row['title']
+        result = pool.apply_async(scrape_reviews, 
+                                  args=(movie_url, movie_title, chrome_driver_path))
+        results.append(result)
 
-        cnt = 0
-        while len(scraper.reviews) < 5 and cnt < 3:
-            scraper = ReviewScraper(movie_url=movie_url, driver = chrome_driver, executable_path=chrome_driver_path,
-                                    movie_title=movie_title)
-            scraper.get_reviews()
-            cnt += 1
-
-        else:
-            all_reviews.extend(scraper.reviews)
-
-    chrome_driver.quit()
-    reviews_df = pd.DataFrame(all_reviews)
+    pool.close()
+    pool.join()
+    print(results)
     
+    all_reviews = []
+    for result in results:
+        all_reviews.extend(result.get())
+        
+    reviews_df = pd.DataFrame(all_reviews)
     output_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    
-    file_path = os.path.join(output_folder, "movie_reviews_total.csv")
+    file_path = os.path.join(output_folder, "movie_reviews_total_mp.csv")
     reviews_df.to_csv(file_path, index=False, encoding='utf-8-sig')
     print(file_path)
-    
-    
+
+
+
+
     
 
 
