2afcef37ea38aa6b39ad394b80a22c75ce3707af
heewoneha <jhjmo0719h@naver.com>
2023-05-12 10:52:29 +0900

    fix: 클라우드 이미지 경로 수정, 포스터 src로 수정, 그 외 코드 정리

--- reviewSite/album/views.py
+++ reviewSite/album/views.py
@@ -1,16 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from braces.views import LoginRequiredMixin, UserPassesTestMixin
 from allauth.account.views import PasswordChangeView
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView,
 )
 from album.models import Album, Comment
 from album.forms import AlbumForm
@@ -23,7 +19,6 @@ class IndexView(ListView):
     context_object_name = 'albums'
     paginate_by = 10
     ordering = ['id'] # 아이디순으로
-
 
 
 class AlbumDetailView(DetailView):
@@ -40,54 +35,6 @@ class AlbumDetailView(DetailView):
         elif vote_type == 'dislike' :
             album.update_negative_votes(request.user)
         return self.get(request,*args,**kwargs)
-
-
-
-class AlbumCreateView(LoginRequiredMixin, CreateView):
-    model = Album
-    form_class = AlbumForm
-    template_name = 'album/album_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('album-detail', kwargs={'album_id': self.object.id})
-
-class AlbumUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Album
-    form_class = AlbumForm
-    template_name = 'album/album_form.html'
-    pk_url_kwarg = 'album_id'
-
-    raise_exception = True
-    
-    def get_success_url(self):
-        return reverse('album-detail', kwargs={'album_id': self.object.id})
-
-    def test_func(self, user):
-        album = self.get_object()
-        return album.author == user
-
-class AlbumDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Album
-    template_name = 'album/album_confirm_delete.html'
-    pk_url_kwarg = 'album_id'
-
-    raise_exception = True
-
-    def get_success_url(self):
-        return reverse('index')
-
-    def test_func(self, user):
-        album = self.get_object()
-        return album.author == user
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['error_message'] = self.request.GET.get('error_message')
-        return context
 
 
 class CustomPasswordChangeView(PasswordChangeView):
